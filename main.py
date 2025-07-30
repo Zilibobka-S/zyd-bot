6a4780c838e14303dc53824627a2fd1e271d64fe
zilibobka <karelovm731@gmail.com>
2025-07-30 16:55:58 +0300

    Main now uses proxies.py automatically

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
+
 #from spys import me, one, filters, proxy_view
+import proxies
 import json
 import time
 import requests
@@ -7,8 +9,13 @@ import requests
 timeout = 3
 a = 0
 proxy_list = []
-proxies={"http": "http://127.0.0.1:1111/", "https": "http://127.0.0.1:1111/"}
+proxies_http={"http": "http://127.0.0.1:1111/", "https": "http://127.0.0.1:1111/"}
 good_proxies = {}
+
+proxies.get_proxy()
+
+
+
 startreal = time.monotonic()
 with open("proxies_list.txt", "r") as f:
     for line in f:
@@ -17,16 +24,16 @@ with open("proxies_list.txt", "r") as f:
 print("array created")
 #print(proxy_list)
 for proxy in proxy_list:
-    proxies["http"] = proxy
-    proxies["https"] = proxy
+    proxies_http["http"] = proxy
+    proxies_http["https"] = proxy
     try:
-        r = requests.get('https://youtu.be/YLhpPghyIQw?si=yKdGR5cTSYvLtlIL', proxies=proxies, timeout=timeout)
+        r = requests.get('https://youtu.be/YLhpPghyIQw?si=yKdGR5cTSYvLtlIL', proxies=proxies_http, timeout=timeout)
         print(r.status_code)
         #print(r.text)
         print(proxy)
         if r.status_code == 200:
             starttime = time.monotonic()
-            rmb = requests.get('https://gist.githubusercontent.com/khaykov/a6105154becce4c0530da38e723c2330/raw/41ab415ac41c93a198f7da5b47d604956157c5c3/gistfile1.txt', proxies=proxies, timeout=5)
+            rmb = requests.get('https://gist.githubusercontent.com/khaykov/a6105154becce4c0530da38e723c2330/raw/41ab415ac41c93a198f7da5b47d604956157c5c3/gistfile1.txt', proxies=proxies_http, timeout=5)
             proxytime = (time.monotonic() - starttime)
             print(proxytime)
             if proxytime < 5:
